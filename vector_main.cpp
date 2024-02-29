9e6a1d5a5eb89c0229dce1d2c6d093dc89a387fa
Mantas Einoris <147090675+mantas001@users.noreply.github.com>
2024-02-29 22:41:29 +0200

    meniu funkcijos pasirinkimo bug fix

--- vector_main.cpp
+++ vector_main.cpp
@@ -10,21 +10,21 @@ using namespace std;
 int main() {
     srand(time(NULL));
     vector<stud> grupe;
-    char pasirinkimas;
+    string pasirinkimas;
     do {
         do {
             cout << "1 - Ivesti duomenis ranka\n2 - Generuoti pazymius\n3 - Generuoti ir pazymius, ir studentu vardus, ir pavardes\n4 - Skaityti duomenis is failo\n5 - Baigti darba\nPasirinkimas: ";            cin >> pasirinkimas;
-        } while (!(pasirinkimas == '1' || pasirinkimas == '2' || pasirinkimas == '3' || pasirinkimas == '4'|| pasirinkimas == '5'));
-        if (pasirinkimas=='1') pasirinkimas1(grupe);
-        else if (pasirinkimas=='2') pasirinkimas2(grupe);
-        else if (pasirinkimas=='3') pasirinkimas3(grupe);
-        else if (pasirinkimas=='4') pasirinkimas4(grupe);
-        else if (pasirinkimas=='5'){
-            cout <<"Programa darba beige" << endl;
+        } while (!(pasirinkimas == "1" || pasirinkimas == "2" || pasirinkimas == "3" || pasirinkimas == "4"|| pasirinkimas == "5"));
+        if (pasirinkimas=="1") pasirinkimas1(grupe);
+        else if (pasirinkimas=="2") pasirinkimas2(grupe);
+        else if (pasirinkimas=="3") pasirinkimas3(grupe);
+        else if (pasirinkimas=="4") pasirinkimas4(grupe);
+        else if (pasirinkimas=="5"){
+            cout <<"Programa darba baige" << endl;
             break;
         }
         else cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
 
-    } while (pasirinkimas != '5');
+    } while (pasirinkimas != "5");
     return 0;
 }
